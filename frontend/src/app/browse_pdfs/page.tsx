9699108bb0370bb2b78cbd8aa0e095d964fd57e4
Shlok Arora <128911480+ShlokArora2709@users.noreply.github.com>
2025-04-11 17:55:24 +0530

    Update page.tsx

--- frontend/src/app/browse_pdfs/page.tsx
+++ frontend/src/app/browse_pdfs/page.tsx
@@ -44,7 +44,7 @@ const DocumentsPage = () => {
   };
 
   return (
-    <div className="min-h-screen p-6 p-6 flex flex-col justify-center items-center">
+    <div className="min-h-screen p-6 p-6 flex flex-col items-center">
       <h1 className="text-3xl font-bold text-center mb-6">Search Documents</h1>
       <p className="text-lg text-gray-600 mb-5 text-center max-w-xl">
         Enter your query below and get the notes generated for you.
